f76a30ffb19ae8d0f5999d8ccc95a55322996699
Tom White <twhite@wpi.edu>
2017-12-23 16:18:32 -0500

    Added basic authentication

--- ifcweb/urls.py
+++ ifcweb/urls.py
@@ -17,12 +17,15 @@ from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    url(r"^$", views.index),
+    url(r"^$", views.index, name='index'),
     url(r'^about/', views.about),   
     url(r'^house/', include('houses.urls')),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}),
+    url(r'^logout/$', auth_views.logout, {'next_page': 'index'}),
     url(r'^admin/', admin.site.urls),
 ]
 
