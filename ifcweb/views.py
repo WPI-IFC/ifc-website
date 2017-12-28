854421117d36bf4c01a5fb789c181c14f4f62b9f
Tom White <twhite@wpi.edu>
2017-12-28 05:00:39 -0500

    Added templating to officer section

--- ifcweb/views.py
+++ ifcweb/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 def index(request):
     return render(request, "index.html")
 
 def about(request):
-    return render(request, "about.html")
+    context = {'officers': [], 'errors': False}
+    groups = [
+        'IFC President',
+        'VP Standards and Internal Operations',
+        'VP Recruitment',
+        'VP Communications',
+        'VP Finance and Service',
+        'Risk Manager',
+        'Academics',
+        'Activities',
+        'Community Involvement',
+        'Community Service',
+        'Membership Development',
+        'Public Relations',
+        'Web',
+    ]
+    for group in groups:
+        try:
+            context['officers'].append((User.objects.filter(groups__name=group)[0], group))
+        except IndexError:
+            print(group)
+            context['errors'] = True
+    return render(request, "about.html", context)
 
 def style(request):
     return render(request, "style-example.html")
