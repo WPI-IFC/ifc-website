010743845ac13d5956eb30692abe7965309fe64d
Tom White <twhite@wpi.edu>
2017-12-30 23:46:28 -0500

    Inital officer page design and dynamic content handling

--- officers/views.py
+++ officers/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
+from django.db.models.query import QuerySet
 
-from .models import Blog, Biography
+from .models import Blog, Biography, Post
 
 def officer_index(request):
     context = {}
@@ -13,4 +14,5 @@ def position_overview(request, slug):
     context['user'] = blog.current_owner
     context['biography'] = Biography.objects.get(user=blog.current_owner)
     context['position'] = blog.position_title
+    context['posts'] = Post.objects.filter(blog=blog)
     return render(request, "position_overview.html", context)
