2c749f193aedeb0a8af48ab1107837ea6cbe7f34
Tom White <twhite@wpi.edu>
2018-02-02 17:40:26 -0500

    Started event structure

---
+++ events/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.conf import settings
+
+from houses.models import Fraternity
+
+class BaseEvent(models.Model):
+    title = models.CharField(max_length=100)
+    d_time = models.DateTimeField()
+    description = models.TextField()
+    splash_img = models.ImageField(upload_to='event_splash/')
+
+    class Meta:
+        abstract = True
+
+class OfficerEvent(BaseEvent):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL
+    )
+
+
+class HouseEvent(BaseEvent):
+    owner = models.ForeignKey(
+        Fraternity,
+        on_delete=models.CASCADE
+    )
