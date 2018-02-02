7220117f9d29a5dea50539876f03db31364c8144
Tom White <twhite@wpi.edu>
2018-02-02 17:40:26 -0500

    Started event structure

---
+++ events/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from . import models
+
+admin.register(models.OfficerEvent)
+admin.register(models.HouseEvent)
--- ifcweb/settings.py
+++ ifcweb/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'ifcweb',
     'houses',
     'officers',
+    'events',
 ]
 
 MIDDLEWARE = [
