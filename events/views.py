ea0875df87e8f054d025a5dd363776ebd3b280dd
Tom White <twhite@wpi.edu>
2018-02-17 21:53:19 -0500

    Events - intermediate push

--- events/views.py
+++ events/views.py
@@ -8,12 +8,13 @@ def event_index(request):
 
 
 def officer_event_info(request, event_id):
-    context = {}
+    context = {"type": "officer"}
     event = get_object_or_404(OfficerEvent, id=event_id)
+    context['splash'] = event.splash_img
     return render(request, "single_event.html", context)
 
 
 def house_event_info(request, event_id):
-    context = {}
+    context = {"type": "house"}
     event = get_object_or_404(HouseEvent, id=event_id)
     return render(request, "single_event.html", context)
