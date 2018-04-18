60ac1fc10f9bacd3f7841d00d0b38500982de2ed
Tom White <twhite@wpi.edu>
2018-04-18 13:43:40 -0400

    Dynamic officer event adding

---
+++ events/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+
+from .models import OfficerEvent, HouseEvent
+
+
+class OfficerEventForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(OfficerEventForm, self).__init__(*args, **kwargs)
+        self.fields["splash_img"].label = "Splash Image"
+        self.fields["d_time"].label = "Date & Time"
+
+    class Meta:
+        model = OfficerEvent
+        fields = ("title", "description", "d_time", "splash_img")
+
+
+class HouseEventForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(OfficerEventForm, self).__init__(*args, **kwargs)
+        self.fields["splash_img"].label = "Splash Image"
+        self.fields["d_time"].label = "Date/Time"
+
+    class Meta:
+        model = HouseEvent
+        fields = ("title", "description", "d_time", "splash_img")
--- events/views.py
+++ events/views.py
@@ -1,15 +1,35 @@
 from datetime import datetime
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required, permission_required
 
 from .models import BaseEvent, OfficerEvent, HouseEvent
+from .forms import OfficerEventForm, HouseEventForm
 
 def event_index(request):
     context = {}
     #TODO(Tom): Change to greater than current datetime
-    context['ifc_events'] = OfficerEvent.objects.filter(d_time__lt=datetime.now()).reverse()
-    context['chapter_events'] = HouseEvent.objects.filter(d_time__lt=datetime.now()).reverse()
+    context['ifc_events'] = OfficerEvent.objects.filter(d_time__gt=datetime.now()).reverse()
+    context['chapter_events'] = HouseEvent.objects.filter(d_time__gt=datetime.now()).reverse()
     return render(request, "events_overview.html", context)
+
+
+@login_required
+@permission_required('events.add_officerevent', raise_exception=True)
+def new_officer_event(request):
+    context = {}
+
+    if request.method == 'GET':
+        context['form'] = OfficerEventForm()
+        context['title'] = "New Event"
+        return render(request, "edit_event.html", context)
+    elif request.method == 'POST':
+        form = OfficerEventForm(request.POST, request.FILES)
+        if form.is_valid():
+            event = form.save(commit=False)
+            event.owner = request.user
+            form.save()
+            return redirect("event-officer-overview", id=event.id)
 
 
 def officer_event_info(request, event_id):
