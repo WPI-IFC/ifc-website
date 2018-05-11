9e68751f702ad616d6bb8707f72ccc2ba8191417
Tom White <twhite@wpi.edu>
2018-05-10 22:16:02 -0400

    Testing - Starting to add tests
    Some examples of how to do testing with Django, have not done any
    real "view" testing for dynamic content yet

--- houses/tests.py
+++ houses/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+from houses.models import Fraternity
 
 # Create your tests here.
+class ModelTests(TestCase):
+
+    def setUp(self):
+        self.test_f = Fraternity.objects.create(
+            english_name="TEST FULL NAME",
+            nickname="TEST_NICKNAME",
+            letters=SimpleUploadedFile("TEST_LETTERS", b"\x00\x01\x02\x03"),
+            primary_color="#FFFFFF",
+            secondary_color="#000000"
+        )
+
+    def test_lower_representation(self):
+        """
+        Test to see if the function correctly converts the english name to a
+        lower representation
+        """
+        self.assertEqual(self.test_f.lower_repr(), 'testfullname')
--- officers/tests.py
+++ officers/tests.py
@@ -1,3 +1,93 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
+from django.utils.timezone import now
 
+from officers.models import Biography,Blog, Post
 # Create your tests here.
+
+class ViewTesting(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="testU", password="test123")
+        self.blog = Blog.objects.create(
+            current_owner=self.user,
+            position_title="TEST_POSITION",
+            slug="test-slug"
+        )
+        self.post = Post.objects.create(
+            blog=self.blog,
+            published=now(),
+            last_edit=now(),
+            author=self.user,
+            author_string=self.user.username,
+            title="TEST_POST_TITLE 1",
+            body="TEST POST BODY 1"
+        )
+        self.client = Client()
+        self.client.login(username="testU", password="test123")
+
+    def test_post_authoring(self):
+        data = {
+            'title': "TEST_POST_TITLE 2",
+            'body': "TEST POST BODY 2"
+        }
+
+        self.client.post("/about/officer/{}/new/".format(self.blog.slug), data)
+
+        posts = Post.objects.all().filter(author=self.user)
+        self.assertEqual(posts.count(), 2)
+
+    def test_unauthorized_post_authoring(self):
+        """
+        Error if the user tries to make a post to a blog that does
+        not belong to them.
+        """
+        User.objects.create_user(username="bad_user", password="test123")
+        self.client.login(username="bad_user", password="test123")
+
+        data = {
+            'title': "TEST_POST_TITLE 2",
+            'body': "TEST POST BODY 2"
+        }
+
+        response = self.client.post("/about/officer/{}/new/".format(self.blog.slug), data)
+        self.assertEqual(response.status_code, 403)
+
+        posts = Post.objects.all().filter(author=self.user)
+        self.assertEqual(posts.count(), 1)
+
+    def test_post_editing(self):
+        data = {
+            'title': "TEST_POST_TITLE 1.5",
+            'body': "TEST POST BODY 1.5"
+        }
+
+        self.client.post("/about/officer/{}/{}/edit/".format(
+            self.post.blog.slug,
+            self.post.id
+        ), data)
+
+        updated_post = Post.objects.get(id=self.post.id)
+        self.assertGreater(updated_post.last_edit, self.post.published)
+        self.assertEquals(updated_post.title, data['title'])
+        self.assertEqual(updated_post.body, data['body'])
+
+    def test_unauthorized_edits(self):
+        User.objects.create_user(username="bad_user", password="test123")
+        self.client.login(username="bad_user", password="test123")
+
+        data = {
+            'title': "TEST_POST_TITLE 1.5",
+            'body': "TEST POST BODY 1.5"
+        }
+
+        response = self.client.post("/about/officer/{}/{}/edit/".format(
+            self.post.blog.slug,
+            self.post.id
+        ), data)
+        self.assertEqual(response.status_code, 403)
+
+        updated_post = Post.objects.get(id=self.post.id)
+        self.assertEqual(updated_post.last_edit, self.post.published)
+        self.assertNotEqual(updated_post.title, data['title'])
+        self.assertNotEqual(updated_post.body, data['body'])
