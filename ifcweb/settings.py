4bc289d22b36a30d723c5635e0e7bb25c4524c18
Tom White <twhite@wpi.edu>
2018-05-12 02:19:10 -0400

    First shot

--- ifcweb/settings.py
+++ ifcweb/settings.py
@@ -77,12 +77,24 @@ WSGI_APPLICATION = 'ifcweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'TRAVIS' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'travisci',
+            'USER': 'postgres',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': ''
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 FIXTURE_DIRS = ["fixtures"]
 
