3f8773550a0ad54445e4f7f684c3f0844af7faca
Joseph Kaming-Thanassi <josephkamingthanassi@gmail.com>
2018-10-14 18:00:09 -0400

    Added Form Validation

--- houses/forms.py
+++ houses/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+import re
 
 from .models import Fraternity
 
@@ -7,3 +8,10 @@ class HouseForm(forms.ModelForm):
     class Meta:
         model = Fraternity
         fields = '__all__'
+
+    def clean_calendar_link(self):
+        data = self.cleaned_data['calendar_link']
+        if not re.match("^https://calendar.google.com/calendar/embed\?src=(.*)", data):
+            raise forms.ValidationError("Link provided is not a google calendar link!")
+        
+        return data
